a0406483faece228bbef41ed25bc4b09443c08f7
Daniel parra <danielparrac26@gmail.com>
2020-10-29 17:20:22 -0500

    routing modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,11 @@ const routes: Routes = [
     path: 'crearCliente',
     component: CrearClienteComponent,
   },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
 ];
 
 @NgModule({
