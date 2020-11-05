d6c3d78548639d19cbcffa4ba35f9977834c7bc0
Daniel parra <danielparrac26@gmail.com>
2020-11-04 22:58:03 -0500

    bg arreglado

---
+++ src/app/models/clientes.ts
@@ -0,0 +1,8 @@
+export class clientesModel {
+  id_cliente: string;
+  nombre: string;
+  documento: string;
+  correo: string;
+  celular: string;
+  direccion: string;
+}
---
+++ src/app/models/producto.ts
@@ -0,0 +1,8 @@
+export class productosModel {
+  id_producto: string;
+  nombre: string;
+  precio: string;
+  imagen: string;
+  celular: string;
+  direccion: string;
+}
