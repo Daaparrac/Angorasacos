cc017bf7d8a34593ae307d0284443b21b867c0ee
Daniel parra <danielparrac26@gmail.com>
2021-02-02 21:00:33 -0500

    Productos finalizado

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceNameService } from '../../services/data.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
 import { ProductosModel } from '../../models/producto';
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
 import { NgForm } from '@angular/forms';
-import { SlugifyPipe } from '../../pipes/slugify.pipe'
+import { SlugifyPipe } from '../../pipes/slugify.pipe';
 
 @Component({
   selector: 'app-productos',
@@ -29,7 +29,8 @@ export class ProductosComponent implements OnInit {
   constructor(
     private datap: ServiceNameService,
     private activatedRoute: ActivatedRoute,
-    private pipeLearn: SlugifyPipe
+    private pipeLearn: SlugifyPipe,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -53,17 +54,12 @@ export class ProductosComponent implements OnInit {
 
   createcodcompleto(texts: string) {
     const textitos1 = `${this.productos.nombre} ${this.productos.color}`;
-    const textitos2 = `${this.productos.talla}`;
     const textitos = `${this.productos.subtotal}`;
     this.productos.codigo = `${this.pipeLearn.transform(
       textitos1
-    )}${this.pipeLearn.transform1(textitos2)} - ${this.pipeLearn.transform2(
+    )} - ${this.pipeLearn.transform2(
       textitos
     )}`;
-  }
-
-  create(texts: string) {
-    console.log(this.productos.talla);
   }
 
   createIvaSub(texts: string) {
@@ -74,24 +70,10 @@ export class ProductosComponent implements OnInit {
       parseInt(textoprecio, 10) -
       (parseInt(textoprecio, 10) * parseInt(textoIVA, 10)) / 100;
     this.productos.subtotal = total;
-    console.log(this.productos.IVA);
-  }
-
-  createTotSub(texts: string) {
-    const textostotal = `${this.productos.subtotal}`;
-    const textoIVA = `${this.productos.IVA}`;
-
-    console.log(textoIVA);
-    console.log(textostotal);
-    this.productos.subtotal = parseInt(this.pipeLearn.preciop(textostotal), 10);
-    console.log(this.productos.subtotal);
   }
 
   guardar(form: NgForm) {
-    console.log(form.invalid);
-
-
-    this.productos.IVA = this.productos.total - this.productos.subtotal;
+    this.productos.Ivap = this.productos.total - this.productos.subtotal;
     this.productos.talla = [];
     this.productos.talla.push(
       {
@@ -139,6 +121,7 @@ export class ProductosComponent implements OnInit {
         icon: 'success',
         text: 'se actualizó',
       });
+      this.router.navigateByUrl('/inventario');
     });
   }
 
